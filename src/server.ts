c4301eb5f3bec592666da7def8602275365390cb
Mohibullah <bullahmohi50@gmail.com>
2026-02-25 14:40:36 +0600

    Refactor server and database connection logic
    Refactored server initialization and database connection logic. Replaced the startServer function with a handler function for incoming requests.

--- src/server.ts
+++ src/server.ts
@@ -1,81 +1,20 @@
-import http, {Server} from 'http'
-import mongoose from 'mongoose';
-import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
+import mongoose from "mongoose";
+import app from "../app";
 
+let isConnected = false;
 
-let server:Server;
+const connectDB = async () => {
+  if (isConnected) return;
 
+  await mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3t5vk.mongodb.net/bongoDB?appName=Cluster0`
+  );
 
+  isConnected = true;
+  console.log("MongoDB connected");
+};
 
-
-async function connectToDb() {
-  try {
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3t5vk.mongodb.net/bongoDB?appName=Cluster0`)
-
-    console.log('db connected')
-
-  } catch (error) {
-    console.log(error,"db connection failed")
-    process.exit(1)
-  }
+export default async function handler(req: any, res: any) {
+  await connectDB();
+  return app(req, res);
 }
-async function startServer() {
-  try {
-    await connectToDb()
-    server = http.createServer(app);
-    const PORT = process.env.PORT || 10000;
-    server.listen(Number(PORT), '0.0.0.0', () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-
-    handleProcessEvents();
-  } catch (error) {
-    console.error("❌ Error during server startup:", error);
-    process.exit(1);
-  }
-}
-
-
-async function gracefulShutdown(signal: string) {
-  console.warn(`🔄 Received ${signal}, shutting down gracefully...`);
-
-  if (server) {
-    server.close(async () => {
-      console.log("✅ HTTP server closed.");
-      
-
-      try {
-        console.log("Server shutdown complete.");
-      } catch (error) {
-        console.error("❌ Error during shutdown:", error);
-      }
-
-      process.exit(0);
-    });
-  } else {
-    process.exit(0);
-  }
-}
-
-/**
- * Handle system signals and unexpected errors.
- */
-function handleProcessEvents() {
-  process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
-  process.on("SIGINT", () => gracefulShutdown("SIGINT"));
-
-  process.on("uncaughtException", (error:any) => {
-    console.error("💥 Uncaught Exception:", error);
-    gracefulShutdown("uncaughtException");
-  });
-
-  process.on("unhandledRejection", (reason:any) => {
-    console.error("💥 Unhandled Rejection:", reason);
-    gracefulShutdown("unhandledRejection");
-  });
-}
-
-// Start the application
-startServer();
